19ac800c19290b3e8d205ae7f3c41d11a27e0858
Andrew Zuniga <azuniga183@gmail.com>
2020-12-09 18:47:36 -0800

    finished 3.1.5

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,13 @@
 var playerName = window.prompt("What is your robot's name?");
-// Note the lack of quotation marks around playerName
-window.alert(playerName);
+// what is this?
+console.log(playerName)
+console.log("This logs a string, good for leaving yourself a message");
+// this will do math and log 20
+console.log(10+10);
+// what is this?
+console.log("Our Robot's name is " + playerName);
+var name = "your name";
+console.log(name);
 
 // this creates a function named "fight"
 
